0460b703a2b02eb5cb7b615fb9301c26046644d0
Idris Shittu <shittuidris45@gmail.com>
2025-04-14 00:39:03 +0100

    Add Unit Tests for RootController

---
+++ src/test/java/uk/gov/hmcts/reform/dev/controllers/RootControllerTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.dev.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RootController.class)
+class RootControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnWelcomeMessage() throws Exception {
+        String expectedMessage = """
+            Welcome to test-backend!
+            Available resources:
+            - API Documentation: /swagger-ui.html
+            - Task Management: /tasks
+            """;
+
+        mockMvc.perform(get("/"))
+               .andExpect(status().isOk())
+               .andExpect(content().string(expectedMessage));
+    }
+
+    @Test
+    void shouldReturnNotFoundForInvalidEndpoint() throws Exception {
+        mockMvc.perform(get("/invalid-endpoint"))
+               .andExpect(status().isNotFound());
+    }
+}
